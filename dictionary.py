9e0fbc7fc87ab54ce7488e409a7c51a8704c6196
veera-vijay <vijayveera506@gmail.com>
2025-12-07 19:28:34 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,32 @@
+a={
+    "name":"vijay",
+    "age" :23,
+    "location":"neyveli"
+}
+print(a)
+# alows keys in duplicate but take recent values
+a={
+    "name":"vijay",
+    "age" :23,
+    "age":"neyveli"
+}
+print(a.keys())
+print(a)
+# values allow duplicate
+b={
+    "name":"vijay",
+    "age" :"neyveli",
+    "location":"neyveli",
+     "location":"neyveli",
+     "location":"neyveli"
+     
+}
+print(b)
+print(b.values())
+
+
+ 
+ 
+
+
+
