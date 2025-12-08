018a74bc1da6ee87e9d3378d5cfd19be69fb381c
veera-vijay <vijayveera506@gmail.com>
2025-12-08 19:18:55 +0530

    Add files via upload

---
+++ positive_or_negative.py
@@ -0,0 +1,9 @@
+num=int(input("enter the number:"))
+if num>1:
+    print("it is a positive number")
+elif num<1:
+    print("it is a negative number")
+else:
+    print("invalid number")
+    
+    
