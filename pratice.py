b17401d57ac3d58451a8633abb7106c75ab4d42a
veera-vijay <vijayveera506@gmail.com>
2025-12-09 18:09:42 +0530

    Add files via upload

---
+++ pratice.py
@@ -0,0 +1,11 @@
+a=int(input("A:"))
+b=int(input("B:"))
+operation=input("add/sub/multiply/divide:")
+if operation=="add":
+    print(a+b)
+elif operation=="sub":
+    print(a-b)
+elif operation=="multiply":
+    print(a*b)
+elif operation=="division":
+    print(a/b)
