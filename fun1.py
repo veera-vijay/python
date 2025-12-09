9515da4d724ef8d83e0a94a5d25b290000312209
veera-vijay <vijayveera506@gmail.com>
2025-12-09 18:14:17 +0530

    Add files via upload

---
+++ fun1.py
@@ -0,0 +1,14 @@
+def add():
+    print("ADDTION:")
+    a=int(input("enter value of a:"))
+    b=int(input("enter value of b:"))
+    print(a+b)
+    
+def sub():
+    print("SUB:")
+    a=int(input("enter value of a:"))
+    b=int(input("enter value of b:"))
+    print(a-b)
+    
+add()
+sub()
